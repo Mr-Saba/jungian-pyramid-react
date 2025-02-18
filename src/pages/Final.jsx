4dabef5d474195b097ec3c089529e67b39ab9a46
Mr-Saba <mirzashvili.saba@gmail.com>
2025-02-18 20:45:44 +0400

    sas

--- src/pages/Final.jsx
+++ src/pages/Final.jsx
@@ -12,6 +12,7 @@ function Final({selectedStageOptions, selectedStageOptionImages, selectedStageAv
 
   const [answer1, setAnswer1] = useState()
   const [answer2, setAnswer2] = useState()
+  const [isFirstAnswered, setIsFirstAnswered] = useState(false)
   const [isFinishContentVisible, setIsFinishContentVisible] = useState(false)
   const [isPdfDownloading, setIsPdfDownloading] = useState(false)
 
@@ -42,29 +43,32 @@ function Final({selectedStageOptions, selectedStageOptionImages, selectedStageAv
             <div className='final__content__outcome'>
               <Pyramid selectedOptions={selectedStageOptions} selectedOptionImages={selectedStageOptionImages} />
               <div className='final__content__outcome__imageContainer'>
-                <img src={`/assets/avatars/${2}.jpg`}/>
+                <img src={selectedStageAvatar}/>
               </div>
             </div>
         }
         <div className={`final__content__questions ${isFinishContentVisible ? 'final' : ''}`}>
-          <div className='final__content__questions__item' >
-            <div className='final__content__questions__cube'></div>
-            <div className='final__content__questions__texts'>
-              <div className={`final__content__questions__texts__period`}>
-                <h1>If you had to summarize your personal brand vision in one powerful sentence, what would it be?</h1>
-                <textarea onChange={(e) => setAnswer1(e.target.value)}></textarea>
+          {(!isFirstAnswered || isFinishContentVisible) &&<div className='final__content__questions__item' >
+              <div className='final__content__questions__cube'></div>
+              <div className='final__content__questions__texts'>
+                <div className={`final__content__questions__texts__period`}>
+                  <h1>If you had to summarize your personal brand vision in one powerful sentence, what would it be?</h1>
+                  <textarea onChange={(e) => setAnswer1(e.target.value)} value={answer1} disabled={isFinishContentVisible}></textarea>
+                </div>
+              </div> 
+            </div>
+          }
+          {(isFirstAnswered || isFinishContentVisible) &&
+            <div className='final__content__questions__item'>
+              <div className='final__content__questions__cube'></div>
+              <div className='final__content__questions__texts'>
+                <div className={`final__content__questions__texts__period`}>
+                  <h1>What specific actions can you take in the next 30 days to start embodying your personal brand?</h1>
+                  <textarea onChange={(e) => setAnswer2(e.target.value)} value={answer2} disabled={isFinishContentVisible}></textarea>
+                </div>
               </div>
             </div> 
-          </div>
-          <div className='final__content__questions__item'>
-            <div className='final__content__questions__cube'></div>
-            <div className='final__content__questions__texts'>
-              <div className={`final__content__questions__texts__period`}>
-                <h1>What specific actions can you take in the next 30 days to start embodying your personal brand?</h1>
-                <textarea onChange={(e) => setAnswer2(e.target.value)}></textarea>
-              </div>
-            </div>
-          </div> 
+          }
         </div>
       </div>
       {!isFinishContentVisible && <img className='final__label' src={Label} />}
@@ -74,6 +78,7 @@ function Final({selectedStageOptions, selectedStageOptionImages, selectedStageAv
         :
         <p className={`final__finishText hide-in-pdf`} onClick={handleDownloadPDF}>download pdf</p>
       }
+      {(!isFirstAnswered && answer1?.trim().length > 0) && <p className={`final__finishText`} onClick={() => {setIsFirstAnswered(true)}}>next</p>}
     </div>
   )
 }
